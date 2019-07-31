2291a60f7a1d10cd7d3ee42124c1eb7a8e5db759
xiaohe1008 <53325710+xiaohe1008@users.noreply.github.com>
2019-07-31 15:25:08 +0800

    the thirld changes

--- src/com/hemaobin/HelloWorld/HelloWorld.java
+++ src/com/hemaobin/HelloWorld/HelloWorld.java
@@ -3,6 +3,7 @@ package com.hemaobin.HelloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
 
         System.out.println("进行一次修改");
         System.out.println("进行一次修改");
